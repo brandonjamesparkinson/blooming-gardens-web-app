bf03e1e8643c29bec5cbbd5ae7460d5b3dbbf097
Brandon Parkinson <brandonjamesparkinson@gmail.com>
2023-06-22 21:36:59 +0100

    + WIP website

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import "./index.css";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Project from "./routes/Project";
+import Contact from "./routes/Contact";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Blooming Gardens</p>
-        <p>Test</p>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/project" element={<Project />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
   );
 }
 
